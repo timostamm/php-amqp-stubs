56014f8cbec0d55906f44c0f7b032986acdd36b4
Timo Stamm <ts@timostamm.de>
2018-10-13 00:54:08 +0200

    Drop everything but stubs.

---
+++ stubs.php
@@ -0,0 +1,24 @@
+<?php
+
+if (!defined('AMQP_NOPARAM')) {
+
+    include "stubs/AMQP.php";
+    include "stubs/AMQPException.php";
+    include "stubs/AMQPBasicProperties.php";
+    include "stubs/AMQPChannel.php";
+    include "stubs/AMQPChannelException.php";
+    include "stubs/AMQPConnection.php";
+    include "stubs/AMQPConnectionException.php";
+    include "stubs/AMQPDecimal.php";
+    include "stubs/AMQPEnvelope.php";
+    include "stubs/AMQPEnvelopeException.php";
+    include "stubs/AMQPExchange.php";
+    include "stubs/AMQPExchangeException.php";
+    include "stubs/AMQPQueue.php";
+    include "stubs/AMQPQueueException.php";
+    include "stubs/AMQPQueue.php";
+    include "stubs/AMQPQueueException.php";
+    include "stubs/AMQPTimestamp.php";
+    include "stubs/AMQPValueException.php";
+
+}
--- tests/amqpqueue_purge_basic.phpt
+++
@@ -1,8 +0,0 @@
---TEST--
-AMQPQueue
---SKIPIF--
-<?php if (!extension_loaded("amqp")) print "skip"; ?>
---FILE--
-<?php
-?>
---EXPECT--
